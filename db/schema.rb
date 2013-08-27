1418c62d211d9f43ba900473353fb069a1f5fb70
Andrew Lorente <andrew.lorente@gmail.com>
2013-08-27 10:55:53 -0700

    Prototype of a vagrant setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130821104034) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
