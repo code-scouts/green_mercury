61ce2e97289b785a70c122c9446ff8d643a8e187
Shawna C Scott <shawnatyger@gmail.com>
2013-09-13 14:26:35 -0700

    remove uneeded 'name' and 'blurb' from footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@ module ApplicationHelper
   def sponsors 
     [
       {
-        name: "",
-        blurb: "",
         logo: "sponsor_logos/janrain_logo.jpg",
         link: "http://www.janrain.com",
       },
