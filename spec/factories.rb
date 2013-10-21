4c5be3b7c47737b6e129805a898ebc536eeb4dc4
Julie Steele & Steven Ferguson <student@epicodus.com>
2013-10-21 12:59:32 -0700

    add location map to events

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :event do
     title 'Awesome Party'
     description 'We will do crazy stuff together'
-    location '123 Cool St.'
+    location '701 E Burnside St  Portland, OR 97214'
     date Date.tomorrow
     start_time Time.now
     end_time (Time.now + 3.hours)
