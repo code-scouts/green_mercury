6e93eb1effd6ce7e276532d2dae88d01bdf3eba9
Julie <julie.d.rose@gmail.com>
2013-10-25 14:46:07 -0700

    remove whitespace - force Travis CI to run again

--- spec/features/concept_features_spec.rb
+++ spec/features/concept_features_spec.rb
@@ -95,7 +95,7 @@ feature 'add a new description to existing concept' do
 
   scenario 'a user adds a valid description with markdown', js: true do 
     fill_in 'concept_description_description', with: "Look what I can do:\n\n ```ruby \n puts 'Hello World!' \n ```"
-    click_button 'Submit New Description'    
+    click_button 'Submit New Description'  
     within('#latest-description') { expect(page).to have_css('.highlight') }
   end
 end
