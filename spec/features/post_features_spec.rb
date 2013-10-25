83ab8514800b1947a4da7d3627e8253da5951041
Steven <steven.ferguson11@gmail.com>
2013-10-25 14:16:49 -0700

    adds context blocks for post specs

--- spec/features/post_features_spec.rb
+++ spec/features/post_features_spec.rb
@@ -9,32 +9,59 @@ feature 'posting on a project' do
     mentor_participation = FactoryGirl.create(:mentor_participation, user_uuid: user.uuid, project: @project)
   end
 
-  scenario 'a project team member creates a valid post on a project' do 
-    visit project_path(@project)
-    click_link 'New Post'
-    fill_in 'post_title', with: 'Does this thing work?'
-    fill_in 'post_content', with: 'Hey does this thing work?'
-    within('form') { click_on 'Submit' }
-    within('#discussion') { page.should have_content 'Does this thing work?'}
+  context 'a project team member' do 
+    scenario 'they create a valid post on a project' do 
+      visit project_path(@project)
+      click_link 'New Post'
+      fill_in 'post_title', with: 'Does this thing work?'
+      fill_in 'post_content', with: 'Hey does this thing work?'
+      within('form') { click_on 'Submit' }
+      within('#discussion') { page.should have_content 'Does this thing work?'}
+    end
+
+    scenario 'they create an invalid post on a project' do 
+      visit project_path(@project)
+      click_link 'New Post'
+      within('form') { click_on 'Submit' }
+      page.should have_content 'error'
+    end
   end
 
-  scenario 'a project team member creates an invalid post on a project' do 
-    visit project_path(@project)
-    click_link 'New Post'
-    within('form') { click_on 'Submit' }
-    page.should have_content 'error'
-  end
+  context 'a user that is not a project participant tries to create a post' do
+    before do 
+      member = new_member
+      ApplicationController.any_instance.stub(:current_user) { member }
+    end
 
-  scenario 'a user that is not a project participant tries to post on a project' do 
-    member = new_member
-    ApplicationController.any_instance.stub(:current_user) { member }
-    visit project_path(@project)
-    page.should_not have_content 'New Post'
+    scenario "they can't see the link" do 
+      visit project_path(@project)
+      page.should_not have_content 'New Post'
+    end
 
-    visit new_post_path(project_id: @project.id)
-    page.should have_content 'Not authorized'
+    scenario "they can't visit the path directly" do 
+      visit new_post_path(project_id: @project.id)
+      page.should have_content 'Not authorized'
+    end
 
-    page.driver.submit :post, posts_path(:post => { :title => 'Hello?', :content => "Anyone home?", :project_id => @project.id }), {}
-    page.should have_content 'Not authorized'
+    scenario "they can't send a POST request directly" do 
+      page.driver.submit :post, posts_path(:post => { :title => 'Hello?', :content => "Anyone home?", :project_id => @project.id }), {}
+      page.should have_content 'Not authorized'
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
