42f9334636557b95557fd9ea8c59936c835cc062
Julie <julie.d.rose@gmail.com>
2013-10-25 14:05:24 -0700

    removed old events controller spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017215601) do
+ActiveRecord::Schema.define(version: 20131015224246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,5 @@ ActiveRecord::Schema.define(version: 20131017215601) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
 end
