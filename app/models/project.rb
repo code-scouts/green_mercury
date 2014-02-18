54138a82cbcf1ae000fd7cb7825bcc1bb99eaa11
Andrew Lorente <andrew.lorente@gmail.com>
2014-02-17 16:36:04 -0800

    Add a required validation
    I don't know why this is only causing errors on production!

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,8 @@ class Project < ActiveRecord::Base
                     styles: { medium: "250x250>", thumb: "75x75>" },
                     default_url: "default_image_:style.png",
                     }.merge(PAPERCLIP_OPTIONS))
+  validates_attachment_content_type :image,
+    :content_type => %w(image/jpeg image/jpg image/png image/gif)
 
   accepts_nested_attributes_for :mentor_participations
   accepts_nested_attributes_for :member_participations
