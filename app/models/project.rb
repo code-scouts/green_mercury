42344ab94764df0283225666877152297a18c27c
Andrew Lorente <andrew.lorente@gmail.com>
2014-02-17 17:52:41 -0800

    Explicit path/url in projects
    I'm not sure why this was necessary, to be honest. It blew up on
    production.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,8 @@ class Project < ActiveRecord::Base
   has_attached_file(:image, {
                     styles: { medium: "250x250>", thumb: "75x75>" },
                     default_url: "default_image_:style.png",
+                    url: '/system/projects/:id/:style.png',
+                    path: 'public/system/projects/:id/:style.png',
                     }.merge(PAPERCLIP_OPTIONS))
   validates_attachment_content_type :image,
     :content_type => %w(image/jpeg image/jpg image/png image/gif)
