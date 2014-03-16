29a7f8a0657c56b4acf673083c02bbc6a6f77897
Ariel Spear <simpleredfox@gmail.com>
2014-03-16 15:58:17 -0700

    I think I've got it

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,10 +1,3 @@
-#It shouldn't be possible to create two concepts with the same name.
-#Create a unique index, validate_uniqueness_of :name,
-#and add a RecordExistsError handler in the ConceptsController.
-#should flash that record exists, including it's name and location
-#and redirect to creating a new page, using information
-#that allready was entered
-
 class ConceptsController < ApplicationController
   def index
     @concepts = Concept.all
@@ -23,12 +16,24 @@ class ConceptsController < ApplicationController
       end
     rescue ActiveRecord::RecordNotUnique => e
       other_concept = Concept.find_by(name: @concept.name)
-      flash[:notice] = "#{@concept.name} already exists: #{concept_url(other_concept)}"
+      flash[:notice] = "#{@concept.name} already exists: #{link_to(concept_url(other_concept))}"
       render 'new'
     end
   end
 
   def show
+    @concept = Concept.includes(:concept_descriptions).find(params[:id])
+    uuids = @concept.concept_descriptions.pluck(:user_uuid)
+    @users = User.fetch_from_uuids(uuids)
+  end
+
+private
+  def concept_params
+    params.require(:concept).permit(:name, concept_descriptions_attributes: [:description, :user_uuid])
+  end
+end
+
+ow
     @concept = Concept.includes(:concept_descriptions).find(params[:id])
     uuids = @concept.concept_descriptions.pluck(:user_uuid)
     @users = User.fetch_from_uuids(uuids)
